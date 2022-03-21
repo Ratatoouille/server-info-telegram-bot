efa7f2c099af00a2fdb2554d70705edf4d9e7b78
Алексей Старцев <startcev.aleksey@gmail.com>
2022-03-21 14:27:32 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"bytes"
+	"log"
+	"os"
+	"os/exec"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+const (
+	bashCommand = "bash"
+)
+
+var (
+	cpuCommand  = []string{"-c", `top -bn2 | grep '%Cpu' | tail -1 | grep -P '(....|...) id,'| awk '{print "CPU usage: " 100-$8 "%"}'`}
+	memCommand  = []string{"-c", `top -bn2 | grep 'MiB Mem' | tail -1 | awk '{printf "Memory usage: %s total; %s free; %s used\n",$4,$6,$8}'`}
+	diskCommand = []string{"-c", "df", "-h"}
+)
+
+func main() {
+	bot, err := tgbotapi.NewBotAPI(os.Getenv("TELEGRAM_APITOKEN"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message == nil {
+			continue
+		}
+
+		if !update.Message.IsCommand() {
+			continue
+		}
+
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
+
+		switch update.Message.Command() {
+		case "cpu":
+			cmd := exec.Command(bashCommand, cpuCommand...)
+			var outb bytes.Buffer
+			cmd.Stdout = &outb
+
+			if err := cmd.Run(); err != nil {
+				log.Println(err)
+				msg.Text = "Oops!"
+			} else {
+				msg.Text = outb.String()
+			}
+		case "mem":
+			cmd := exec.Command(bashCommand, memCommand...)
+			var outb bytes.Buffer
+			cmd.Stdout = &outb
+
+			if err := cmd.Run(); err != nil {
+				log.Println(err)
+				msg.Text = "Oops!"
+			} else {
+				msg.Text = outb.String()
+			}
+		case "disk":
+			cmd := exec.Command(bashCommand, diskCommand...)
+			var outb bytes.Buffer
+			cmd.Stdout = &outb
+
+			if err := cmd.Run(); err != nil {
+				log.Println(err)
+				msg.Text = "Oops!"
+			} else {
+				msg.Text = outb.String()
+			}
+		default:
+			msg.Text = "Oops!"
+		}
+
+		if _, err := bot.Send(msg); err != nil {
+			log.Println(err)
+		}
+	}
+}
